7188615406fcea8fd15618d3499a80f62e0d2063
本朋 <benpeng.jbp@alibaba-inc.com>
2017-06-15 19:41:23 +0800

    modify log

---
+++ src/main/groovy/com/afterecho/loglifecycle/LogLifeCyclePluginExtension.groovy
@@ -0,0 +1,10 @@
+package com.afterecho.loglifecycle
+
+/**
+ * Created by jiangbenpeng on 15/06/2017.
+ * @author benpeng.jiang
+ * @version 1.0.0
+ */
+class LogLifeCyclePluginExtension {
+    boolean debug
+}
--- src/main/groovy/com/brianattwell/plugin/ClassTransformer.java
+++ src/main/groovy/com/brianattwell/plugin/ClassTransformer.java
@@ -1,13 +1,15 @@
 package com.brianattwell.plugin;
 
-import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.NotFoundException;
 import javassist.build.IClassTransformer;
 import javassist.build.JavassistBuildException;
 
+import static com.brianattwell.plugin.utils.ClassInjectUtil.getOnCreateMethod;
+import static com.brianattwell.plugin.utils.ClassInjectUtil.insertLog;
 import static com.github.stephanenicolas.morpheus.commons.JavassistUtils.isActivity;
+import static com.github.stephanenicolas.morpheus.commons.JavassistUtils.isFragment;
 
 public class ClassTransformer implements IClassTransformer {
 
@@ -15,12 +17,12 @@ public class ClassTransformer implements IClassTransformer {
     public void applyTransformations(CtClass ctClass) throws JavassistBuildException {
         System.out.println(":plugin:applyTransformations on " + ctClass.getName());
         try {
-            CtMethod[] methods = getOnCreateMethod(ctClass);
-            for (CtMethod method : methods) {
-                method.insertAfter("android.util.Log.d(\"MOO\", \"I am inserted code!\");");
+            if (isAnActivity(ctClass)) {
+                CtMethod[] methods = getOnCreateMethod(ctClass);
+                for (CtMethod method : methods) {
+                    insertLog(method);
+                }
             }
-
-//            insertLog(ctClass);
         } catch (Exception e) {
             throw new JavassistBuildException(e);
         }
@@ -29,7 +31,7 @@ public class ClassTransformer implements IClassTransformer {
     @Override
     public boolean shouldTransform(CtClass candidateClass) throws JavassistBuildException {
         try {
-            return isAnActivity(candidateClass);
+            return isAnActivity(candidateClass) || isFragment(candidateClass);
         } catch (Exception e) {
             throw new JavassistBuildException(e);
         }
@@ -38,34 +40,6 @@ public class ClassTransformer implements IClassTransformer {
     private static boolean isAnActivity(CtClass candidateClass) throws NotFoundException {
         // TODO: handle support activity
         return isActivity(candidateClass);
-    }
-
-    public static void insertLog(CtClass cc) throws Exception {
-        CtMethod[] ctMethods = cc.getMethods();
-        if (ctMethods != null) {
-            for (int i = 0; i < ctMethods.length; i++) {
-                CtMethod ctMethod = ctMethods[i];
-                if (cc.equals(ctMethod.getDeclaringClass())) {
-                    String methodName = ctMethod.getLongName();
-                    ctMethod.insertBefore("{ System.out.println(\"" + methodName + ": before\"); }");
-                    ctMethod.insertAfter("{ System.out.println(\"" + methodName + ": end\"); }");
-                }
-            }
-        }
-
-        cc.writeFile();
-    }
-
-    private CtMethod[] getOnCreateMethod(CtClass ctClass)
-            throws NotFoundException {
-        String className = ctClass.getName();
-        ClassPool pool = ctClass.getClassPool();
-
-        CtMethod c1 = pool.get(className).getMethod("onCreate",
-                "(Landroid/os/Bundle;Landroid/os/PersistableBundle;)V");
-        CtMethod c2 = pool.get(className).getMethod("onCreate",
-                "(Landroid/os/Bundle;)V");
-        return new CtMethod[] {c1, c2};
     }
 }
 
